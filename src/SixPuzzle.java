bfeab521fdf9ec794cd288caf9835e5d4b3dde0e
Arian Omidi <arian.omidi@icloud.com>
2021-02-04 16:44:19 -0600

    Get children completed for State class

--- src/SixPuzzle.java
+++ src/SixPuzzle.java
@@ -3,7 +3,7 @@ public class SixPuzzle {
     private State initState, goalState;
     private State curState, prevState;
 
-    public SixPuzzle(int[][] initState, int[][] goalState){
+    public SixPuzzle(int[] initState, int[] goalState){
         this.initState = this.curState = this.prevState = new State(initState);
         this.goalState = new State(goalState);
     }
--- src/State.java
+++ src/State.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
-public class State {
+public class State implements Comparable<State> {
 
-    private int emptyPos;
+    private int emptyIndex;
     private int[] state;
 
     public State(int[] state){
@@ -10,32 +12,24 @@ public class State {
 
         for (int i = 0; i < this.state.length; i++){
             if (this.state[i] == 0){
-                emptyPos = i;
+                this.emptyIndex = i;
                 break;
             }
         }
     }
 
-    public State(int[][] state){
-        int[] arr = new int[6];
+    /* Getters */
 
-        for (int i = 0; i < 6; i++){
-            arr[i] = state[i % 2][i % 3];
-        }
-
-        new State(arr);
-    }
-
-    public int getEmptyPos() {
-        return emptyPos;
+    public int getEmptyIndex() {
+        return emptyIndex;
     }
 
     public int[] getState() {
         return state;
     }
 
-    public int[] getNeighbors() {
-        switch (emptyPos){
+    private int[] getNeighbors() {
+        switch (emptyIndex){
             case 0:
                 return new int[]{1,4};
             case 1:
@@ -51,6 +45,35 @@ public class State {
             default:
                 return new int[]{};
         }
+    }
+
+    public List<State> getChildren(){
+        int[] neighbors = getNeighbors();
+        ArrayList<State> children = new ArrayList<>();
+
+        for (int i = 0; i < neighbors.length; i++){
+            State child = new State(this.state);
+            child.swapValues(neighbors[i]);
+
+            for (int j = 0; j <= children.size(); j++){
+                if (i == children.size()){
+                    children.add(child);
+                    break;
+                } else if (children.get(j).getState()[emptyIndex] > child.getState()[emptyIndex]){
+                    children.add(j, child);
+                    break;
+                }
+            }
+        }
+
+        return children;
+    }
+
+    public void swapValues(int index){
+        int tmp = state[index];
+
+        state[emptyIndex] = state[index];
+        state[index] = 0;
     }
 
     public boolean equals(State state1){
